95cd1e0c968662f38d69cf7bc411a496bdf69a95
Denis Derkach <derkach.denis@lll.kpi.ua>
2024-10-16 15:43:30 +0300

    Bug fix. Update task adding and task opening

--- src/main/java/ua/kpi/edutrackerstudent/repository/StudentsTaskRepository.java
+++ src/main/java/ua/kpi/edutrackerstudent/repository/StudentsTaskRepository.java
@@ -13,9 +13,9 @@ import ua.kpi.edutrackerentity.entity.enums.StatusStudentsTask;
 import java.util.List;
 
 public interface StudentsTaskRepository extends JpaRepository<StudentsTask, Long>, JpaSpecificationExecutor<StudentsTask> {
-    @Query("select distinct s.task from StudentsTask s where lower(s.task.name) like lower(CONCAT('%', :taskName, '%')) and s.student.id = :studentId")
+    @Query("select distinct s.task, s.id from StudentsTask s where lower(s.task.name) like lower(CONCAT('%', :taskName, '%')) and s.student.id = :studentId")
     Page<Task> findAllTaskByTaskNameLikeAndStudentId(String taskName, Long studentId, Pageable pageable);
-    @Query("select distinct s.task.course from StudentsTask s where lower(s.task.course.name) like lower(CONCAT('%', :courseName, '%')) and s.student.id = :studentId")
+    @Query("select distinct s.task.course, s.id from StudentsTask s where lower(s.task.course.name) like lower(CONCAT('%', :courseName, '%')) and s.student.id = :studentId")
     Page<Course> findAllCourseByCourseNameLikeAndStudentId(String courseName, Long studentId, Pageable pageable);
     @Query("SELECT COALESCE(SUM(st.mark), 0) FROM StudentsTask st WHERE st.student.id = :studentId AND st.task.course.id = :courseId")
     Long countMarkByStudentIdAndCourseId(Long studentId, Long courseId);
